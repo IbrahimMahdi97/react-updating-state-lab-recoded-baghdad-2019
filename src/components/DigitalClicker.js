ad45379d17b16a5b024b36565048e94c1bf55d09
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-17 16:46:04 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -14,7 +14,7 @@ class DigitalClicker extends React.Component {
   
   handleClick = () => {
     this.setState(prevState => {
-      timesClicked: prevState.timesClicked+1
+      timesClicked: prevState.timesClicked + 1
     })
   }
   
