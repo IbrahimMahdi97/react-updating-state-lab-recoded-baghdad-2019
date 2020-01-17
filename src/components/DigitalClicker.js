bcf2d424a32eea67b3281d27e43f585337d2f08a
Ibrahim Mahdi <ibrahimmahdi115@gmail.com>
2020-01-17 16:46:49 +0000

    Automatically backed up by Learn

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -2,26 +2,24 @@
 import React from 'react';
 
 class DigitalClicker extends React.Component {
-  
   constructor() {
-    
     super();
-    
+
     this.state = {
       timesClicked: 0,
     };
   }
-  
+
   handleClick = () => {
-    this.setState(prevState => {
-      timesClicked: prevState.timesClicked + 1
-    })
+    this.setState(prevState => ({
+      timesClicked: prevState.timesClicked+1
+    }))
   }
-  
+
   render() {
-    return(
+    return (
       <button onClick={this.handleClick}>{this.state.timesClicked}</button>
-      )
+    );
   }
 }
 
